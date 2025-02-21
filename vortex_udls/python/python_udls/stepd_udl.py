b7ff3ebec2996f2499a1230a87d1c688a2690c42
TY373 <TY373@node5.vortexexp.cascadefuse-pg0.wisc.cloudlab.us>
2025-02-20 21:52:07 -0600

    all trigger true

--- vortex_udls/python/python_udls/stepd_udl.py
+++ vortex_udls/python/python_udls/stepd_udl.py
@@ -271,7 +271,7 @@ class StepDUDL(UserDefinedLogic):
         shard_index = 0
         
         self.capi.put(f"/stepE/stepD_{batch_id}", res_json_byte, subgroup_type=subgroup_type,
-                subgroup_index=subgroup_index,shard_index=STEPD_NEXT_UDL_SHARD_INDEX, message_id=1)
+                subgroup_index=subgroup_index,shard_index=STEPD_NEXT_UDL_SHARD_INDEX, message_id=1,trigger=True)
         
         
         # garbage cleaning via emit and del
